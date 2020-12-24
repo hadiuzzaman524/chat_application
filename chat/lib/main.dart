b10808e1d6b7257f90cddaa724b2480c152b06c8
Md. Hadiuzzaman <52348628+hadiuzzaman524@users.noreply.github.com>
2020-12-24 15:19:06 +0600

    working with StreamBuilder and Cloud Firestore

--- chat/lib/main.dart
+++ chat/lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import './screens/auth.dart';
 import './screens/chat.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -10,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: AuthScreen(),
+      home: ChatScreen(),
       routes: {
         AuthScreen.routeName: (context) => AuthScreen(),
         ChatScreen.routeName: (context) => ChatScreen(),
--- chat/lib/screens/chat.dart
+++ chat/lib/screens/chat.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatefulWidget {
-  static const routeName='/ChatScreen';
+  static const routeName = '/ChatScreen';
 
   @override
   _ChatScreenState createState() => _ChatScreenState();
@@ -14,12 +16,35 @@ class _ChatScreenState extends State<ChatScreen> {
       body: Column(
         children: [
           Expanded(
-            child: ListView.builder(itemBuilder: (context,index){
-              return Text("Love u mantasha");
-            },
-            itemCount: 10,),
+            child: StreamBuilder<QuerySnapshot>(
+              // ignore: deprecated_member_use
+              stream: Firestore.instance
+                  .collection('chats/qg9zAvl4VbAcZ56odH7J/msg')
+                  .snapshots(),
+              builder: (ctx, snapShoot) {
+                return snapShoot.connectionState == ConnectionState.waiting
+                    ? Center(
+                        child: CircularProgressIndicator(),
+                      )
+                    : ListView.builder(
+                        itemBuilder: (context, index) {
+                          return Text(snapShoot.data.docs[index]['text']);
+                        },
+                        itemCount: snapShoot.data.size,
+                      );
+              },
+            ),
           ),
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.expand_less),
+        onPressed: () {
+          // ignore: deprecated_member_use
+          Firestore.instance.collection('chats/qg9zAvl4VbAcZ56odH7J/msg').add({
+            'text': 'my name is hadiuzzaman',
+          });
+        },
       ),
     );
   }
